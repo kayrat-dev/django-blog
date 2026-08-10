732e50f53ca5e9c71667c541ff867c741349a096
Кайрат <kayrattad@gmail.com>
2026-08-10 20:34:58 +0600

    Configure django-environ, add .env support and fix prod settings

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env = environ.Env()
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -1,7 +1,5 @@
 from .base import *
 
-# На сервере отладка КАТЕГОРИЧЕСКИ запрещена
 DEBUG = False
 
-# Домен твоего будущего сервера (можно вынести в .env)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['yourdomain.com'])
