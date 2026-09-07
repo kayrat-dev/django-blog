713de59ca4ca677e49b13993e39ffd60dbbacc59
Кайрат <kayrattad@gmail.com>
2026-09-07 18:31:22 +0600

    Move environment configuration to project root

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env()
 
-environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR.parent / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
